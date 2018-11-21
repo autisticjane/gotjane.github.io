bedce664033d8f2af765a071f217c5383145f12a
Jane Lawson <gotjane@users.noreply.github.com>
2018-11-20 20:25:06 -0600

    Create index.php

---
+++ sandbox/hopefades/index.php
@@ -0,0 +1,98 @@
+<!doctype html-->
+
+<head>
+	<title>Hope Fades</title> <!-- Enter your name inside the <title></title> tags-->
+	<!--DON'T TOUCH THIS-->
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="bootstrap.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="animate.css">
+	<link rel="shortcut icon" href="favi.png"> <!--Upload your favicon called "favi.png". It is the little picture in the tab 											 of the browser.-->
+	<link rel="stylesheet" href="social/css/social.css">
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900' rel='stylesheet' type='text/css'>
+
+	<meta name="viewport" content="width=device-width, initial-scale =1.0, user-scaleable = no">
+
+</head>
+
+<body class="animated fadeIn">
+
+	<section id="intro">
+
+		<div class="intro-container">
+
+			<!--Time for editing your new website, so here between the <h1></h1> tags you can add your intro text.-->
+
+			<h1><?php include("test3.php"); ?></h1>
+
+			<!--Icons-->
+
+			<!-- Let's go on with your links. Here are 4 different icons, if you want to change an icon into another, here is an list of icons you can use: cloud
+										mail
+										feather
+										twitter
+										facebook
+										dribbble
+										rdio
+										instagram
+										globe
+
+			Basically add this words into the class between the <i></i> tags.-->
+
+			<div class="icons animated fadeInDown">
+
+				<a class="twitter" href="http://twitter.com/hopefadesorg"><i class="icon-twitter"></i></a>
+
+				<a class="facebook" href="http://facebook.com/hopefadesorg"><i class="icon-facebook"></i></a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<!--About-->
+
+	<section id="about">
+
+		<div class="container">
+
+			<h2>About<span class="icon"><i class="icon-user-1"></i></span></h2>
+
+			<!-- Let's edit the about text, basically add your about text between the <h3></h3> tags.-->
+
+			<h3>Hope Fades displays <span class="available">one quote or phrase per hour</span> to inspire and promote life and hope. Sometimes the words someone sees are the only or the last words they see all day, and sometimes that small amount of hope is all they need to keep them going. This site is not a place of judgment, so you'll never see that here.</h3>
+
+		</div>
+
+	</section>
+
+<!--	<section id="contact">
+
+		<div class="container">
+
+			<h2>Press<span class="icon"><i class="icon-comment-1"></i></span></h2>
+
+			 here is the contact section, now add the text you want to use in your about section.
+
+			<h3>If you mention/write about this website, please email Liz the link from her contact form at <a class="link" href="https://janepedia.com/contact">Janepedia</a>.</h3>
+
+		</div>
+
+	</section>-->
+
+		<section id="Hardware">
+
+		<div class="container">
+
+			<h2><span class="icon"><i class="icon-desktop"></i></span></h2>
+
+			<!-- here is the Hardware section, now add the text you want to use in your hardware section.-->
+
+			<h3>&copy; 2012&mdash;present. Hope Fades was founded by <a class="link" href="https://janepedia.com">Liz</a> and is hosted by <a class="link" href="https://ofblue.org">OfBlue</a>. All quotes listed in <a class="link" href="allphrases.txt">the archive</a>.</a></h3>
+
+		</div>
+
+	</section>
+
+</body>
